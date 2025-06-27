9f517159c785f00dee320dbe7b6f2c3d5d66b08e
bamtoldotol <121457248+bamtoldotol@users.noreply.github.com>
2025-06-28 06:25:57 +0900

    timeboxer_v0.5
    timerboxer_working_version_0.5 (improved ios text input at home, timetable screen)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "1631965726"
+    serviceWorkerVersion: "2242375094"
   }
 });
