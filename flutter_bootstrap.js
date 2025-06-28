0cc1e182863077c2872d7bffa7b497b0c9f676de
bamtoldotol <121457248+bamtoldotol@users.noreply.github.com>
2025-06-28 21:08:46 +0900

    timeboxer_v0.62
    timeboxer_working_version_v0.62 (fixing text input bug)

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -39,6 +39,6 @@ _flutter.buildConfig = {"engineRevision":"8cd19e509d6bece8ccd74aef027c4ca9473630
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "2990785868"
+    serviceWorkerVersion: "1791184438"
   }
 });
